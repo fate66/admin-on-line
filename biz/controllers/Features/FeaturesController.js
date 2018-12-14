254cf333f13045c7d7f0d14579a0b40729dc5aa5
suilingbao <suilingbao@weidian.com>
2018-12-14 17:22:44 +0800

    alert code

--- biz/controllers/Features/FeaturesController.js
+++ biz/controllers/Features/FeaturesController.js
@@ -5,8 +5,51 @@ export default class Features {
     uploadFiles(ctx) {
         var execSync = require('child_process').execSync;
         var cmdNpm = 'npm run build'
-        let res = execSync(cmdNpm, {cwd: '/Users/ft/Desktop/code/myproject/web-admin'})
-        let str = `<div style="white-space: pre-wrap;">${res.toString()}</div>`
-        ctx.response.body = str
+
+
+        let error = ''
+        let res = ''
+        try {
+            execSync('git pull', {cwd: '/root/web-admin'})
+        } catch (e) {
+            ctx.infoLog.info(e)
+            error = e
+        }
+        if (!error) {
+            try {
+                res = execSync(cmdNpm, {cwd: '/root/web-admin'})
+            } catch (e) {
+                ctx.infoLog.info(e)
+                error = e
+            }
+        }
+        if (!error) {
+            try {
+                execSync('cp -rf /root/web-admin/dist/* /root/admin-server/static')
+            } catch (e) {
+                ctx.infoLog.info(e)
+                error = e
+            }
+        }
+        if (error) {
+            let cache = []
+            error = JSON.stringify(error, (key, val) => {
+                if (val && typeof val === 'object') {
+                    if (cache.indexOf(val) !== -1) {
+                        try {
+                            return JSON.parse(JSON.stringify(val))
+                        } catch (e) {
+                            return 'Circular'
+                        }
+                    }
+                    cache.push(val)
+                }
+                return val
+            })
+            cache = ''
+            ctx.response.body = error
+        } else {
+            ctx.response.body = `<div style="white-space: pre-wrap;">${res.toString()}</div>`
+        }
     }
 }
