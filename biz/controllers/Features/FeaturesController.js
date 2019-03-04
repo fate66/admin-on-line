108892afe09897a94f9567c7772d40d2397de3f5
suilingbao <suilingbao@weidian.com>
2019-03-04 15:04:09 +0800

    alert json.string

--- biz/controllers/Features/FeaturesController.js
+++ biz/controllers/Features/FeaturesController.js
@@ -1,57 +1,64 @@
 export default class Features {
-    constructor() {
-
+  constructor() {
+  
+  }
+  
+  uploadFiles(ctx) {
+    var execSync = require('child_process').execSync;
+    var cmdNpm = 'npm run build'
+    
+    let error = ''
+    let res = ''
+    try {
+      let pull = execSync('git pull', {cwd: '/root/web-admin'})
+      ctx.infoLog.info('Git pull：', pull.toString())
+    } catch (e) {
+      ctx.infoLog.info(e)
+      error = e
     }
-    uploadFiles(ctx) {
-        var execSync = require('child_process').execSync;
-        var cmdNpm = 'npm run build'
-
-        let error = ''
-        let res = ''
-        try {
-            let pull = execSync('git pull', {cwd: '/root/web-admin'})
-            ctx.infoLog.info('Git pull：', pull.toString())
-        } catch (e) {
-            ctx.infoLog.info(e)
-            error = e
-        }
-        if (!error) {
-            try {
-                res = execSync(cmdNpm, {cwd: '/root/web-admin'})
-                ctx.infoLog.info('npm run build：', res)
-            } catch (e) {
-                ctx.infoLog.info('npm run build error:', e)
-                error = e
-            }
-        }
-        if (!error) {
-            try {
-                let cp = execSync('cp -rf /root/web-admin/dist/* /root/admin-server/static')
-                ctx.infoLog.info('cp结束')
-            } catch (e) {
-                ctx.infoLog.info('cp error:', e)
-                error = e
-            }
-        }
-        if (error) {
-            let cache = []
-            error = JSON.stringify(error, (key, val) => {
-                if (val && typeof val === 'object') {
-                    if (cache.indexOf(val) !== -1) {
-                        try {
-                            return JSON.parse(JSON.stringify(val))
-                        } catch (e) {
-                            return 'Circular'
-                        }
-                    }
-                    cache.push(val)
-                }
-                return val
-            })
-            cache = ''
-            ctx.response.body = error
-        } else {
-            ctx.response.body = `<div style="white-space: pre-wrap;">${res.toString()}</div>`
-        }
+    if (!error) {
+      try {
+        res = execSync(cmdNpm, {cwd: '/root/web-admin'})
+        ctx.infoLog.info('npm run build：', res)
+      } catch (e) {
+        ctx.infoLog.info('npm run build error:', e)
+        error = e
+      }
     }
+    if (!error) {
+      try {
+        let cp = execSync('cp -rf /root/web-admin/dist/* /root/admin-server/static')
+        ctx.infoLog.info('cp结束')
+      } catch (e) {
+        ctx.infoLog.info('cp error:', e)
+        error = e
+      }
+    }
+    if (error) {
+      let cache = []
+      error = JSON.stringify(error, (key, val) => {
+        if (val && typeof val === 'function') {
+          Function.prototype.toString.call(val)
+        }
+        if (!val) {
+          return ''
+        }
+        if (val && typeof val === 'object') {
+          if (cache.indexOf(val) !== -1) {
+            try {
+              return JSON.parse(JSON.stringify(val))
+            } catch (e) {
+              return 'Circular'
+            }
+          }
+          cache.push(val)
+        }
+        return val
+      })
+      cache = ''
+      ctx.response.body = error
+    } else {
+      ctx.response.body = `<div style="white-space: pre-wrap;">${res.toString()}</div>`
+    }
+  }
 }
